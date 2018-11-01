0690bf0b93bbc7b5b2838816ff7eda69de85da1a
ehdi <i@ehdi.in>
2018-11-01 19:28:33 +0330

    Redirect unsubscribe user to plan page

---
+++ src/main/webapp/src/app/blocks/interceptor/subscription-expired.interceptor.ts
@@ -0,0 +1,26 @@
+import { Injector } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { LoginService } from 'app/core/login/login.service';
+
+export class SubscriptionExpiredInterceptor implements HttpInterceptor {
+  constructor(private injector: Injector) {}
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(
+      tap(
+        (event: HttpEvent<any>) => {},
+        (err: any) => {
+          if (err instanceof HttpErrorResponse) {
+            if (err.status === 402) {
+              const loginService: LoginService = this.injector.get(LoginService);
+              loginService.subscription();
+            }
+          }
+        }
+      )
+    );
+  }
+
+}
