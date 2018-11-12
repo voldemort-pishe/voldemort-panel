91410dc8a0bad0096891016e8ffa06d585762136
ehdi <i@ehdi.in>
2018-11-12 20:42:34 +0330

    Remove example

--- src/main/webapp/src/app/examples/form/form.selectors.ts
+++
@@ -1,8 +0,0 @@
-import { createSelector } from '@ngrx/store';
-
-import { ExamplesState, selectExamples } from '@app/examples/examples.state';
-
-export const selectForm = createSelector(
-  selectExamples,
-  (state: ExamplesState) => state.form
-);
