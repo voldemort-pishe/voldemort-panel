74fb249f291e421791c9ceadfecbf19473e358e5
ehdi <i@ehdi.in>
2018-11-17 00:30:20 +0330

    Implement candidate service

---
+++ src/main/webapp/src/app/core/services/candidate.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { environment as env } from '@env/environment';
+import {Candidate} from "@app/shared/model/candidate.model";
+
+type EntityArrayResponseType = HttpResponse<Candidate>;
+
+@Injectable({ providedIn: 'root' })
+export class CandidateService {
+
+  private resourceUrl = env.serverApiUrl + 'api/candidate';
+
+  constructor(private http: HttpClient) {}
+
+  loadAll(): Observable<EntityArrayResponseType> {
+    return this.http.get<Candidate>(`${this.resourceUrl}/company`, { observe: 'response' });
+  }
+
+  search(param: string): Observable<EntityArrayResponseType> {
+    return this.http.get<Candidate>(`${this.resourceUrl}/company?${param}`, { observe: 'response' });
+  }
+
+
+}
