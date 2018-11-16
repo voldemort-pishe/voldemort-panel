9631138ba8845022cd6b701e68db4272d493264d
ehdi <i@ehdi.in>
2018-11-17 00:30:39 +0330

    Implement company pipeline service

---
+++ src/main/webapp/src/app/core/services/company-pipeline.service.ts
@@ -0,0 +1,22 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpResponse} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+import {environment as env} from '@env/environment';
+import {CompanyPipelineVm} from "@app/shared/model/company-pipeline-vm.model";
+
+type EntityArrayResponseType = HttpResponse<CompanyPipelineVm>;
+
+@Injectable({ providedIn: 'root' })
+export class CompanyPipelineService {
+
+  private resourceUrl = env.serverApiUrl + 'api/company-pipeline';
+
+  constructor(private http: HttpClient) {}
+
+  loadAll(): Observable<EntityArrayResponseType> {
+    return this.http.get<CompanyPipelineVm>(`${this.resourceUrl}/company`, { observe: 'response' });
+  }
+
+
+}
