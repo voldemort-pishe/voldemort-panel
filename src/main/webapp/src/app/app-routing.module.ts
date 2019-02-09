47f339c1f2c4098203cfb32273038b48998bff78
Eiman Iranpour <eiman.iranpour@gmail.com>
2019-02-09 20:00:20 +0330

    Removed hash from urls.

--- src/main/webapp/src/app/app-routing.module.ts
+++ src/main/webapp/src/app/app-routing.module.ts
@@ -111,8 +111,7 @@ const routes: Routes = [
   // useHash supports github.io demo page, remove in your app
   imports: [
     RouterModule.forRoot(routes, {
-      useHash: true,
-      scrollPositionRestoration: 'enabled'
+       scrollPositionRestoration: 'enabled'
     })
   ],
   exports: [RouterModule]
