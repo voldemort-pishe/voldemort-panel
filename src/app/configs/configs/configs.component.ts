e7e71b2a646ba6d390c6f80ab38471551ddcb39c
Eiman Iranpour <eiman.iranpour@gmail.com>
2019-06-05 22:27:34 +0430

    Update configs navigation items as side menu.

--- src/app/configs/configs/configs.component.ts
+++ src/app/configs/configs/configs.component.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfigsComponent implements OnInit {
 
+  menuItems: { title: string; url: string; }[] = [
+    {
+      title: 'اطلاعات شرکت',
+      url: 'company-info',
+    },
+    {
+      title: 'مدیریت اعضا',
+      url: 'company-members',
+    },
+    {
+      title: 'تراکنش‌های مالی',
+      url: 'transactions',
+    },
+  ];
+
   constructor() { }
 
   ngOnInit() {
