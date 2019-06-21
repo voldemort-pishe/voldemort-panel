71fe62b2da3c50579717d6a6e3454a78ffd70f8a
Eiman Iranpour <eiman.iranpour@gmail.com>
2019-06-21 22:32:45 +0430

    Implement hasPermissions method in account service.

--- src/app/shared/services/data/account.service.ts
+++ src/app/shared/services/data/account.service.ts
@@ -4,11 +4,17 @@ import { ApiService, ApiResponse } from '../api.service';
 import { UserModel } from '@app/shared/model/user.model';
 import { MessageModel } from '@app/shared/model/message.model';
 import { CredentialModel } from '@app/shared/model/credential.model';
+import { CacheService } from '../cache.service';
+import { Permission } from '@app/shared/model/enumeration/permission';
+import { map } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class AccountService {
 
-    constructor(private api: ApiService) { }
+    constructor(
+        private api: ApiService,
+        private cache: CacheService,
+    ) { }
 
     login(data: any): Observable<ApiResponse<CredentialModel>> {
         return this.api.post<CredentialModel>('account/authenticate', data);
@@ -27,10 +33,25 @@ export class AccountService {
     }
 
     get(): Observable<ApiResponse<UserModel>> {
-        return this.api.get<UserModel>('account');
+        return this.cache.getDataOnce<UserModel>('account');
     }
 
     save(account: any): Observable<ApiResponse<void>> {
         return this.api.post<void>('account', account);
+    }
+
+    hasPermissions(...permissions: Permission[]): Observable<boolean> {
+        return this.get().pipe(map(r => {
+            if (!r.success) return true;
+            if (!r.data.authorities) return false;
+
+            const allPermissions: Permission[] = r.data.authorities
+                .map(a => a.permissions ? a.permissions : [])
+                .reduce((a, b) => a.concat(b));
+
+            return permissions
+                .map(p => !!allPermissions.find(_p => p === _p))
+                .reduce((a, b) => a && b);
+        }));
     }
 }
